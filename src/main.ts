16b53e1e4a5df927a47943e3b4e6b0b9acb6d7e5
Your Name <you@example.com>
2026-08-14 22:10:59 +0300

    last Update on main js

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import './assets/styles.css'
-import './assets/main2.css'
+import "./assets/styles.css";
+import "./assets/main2.css";
 
+import "./assets/main.js";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
